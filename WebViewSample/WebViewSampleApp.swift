e0ad04ca4601221022741113525d37d31eae446f
fummicc1 <fumiya.tennis1@gmail.com>
2023-01-23 22:30:14 +0900

    Initial Commit

---
+++ WebViewSample/WebViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewSampleApp.swift
+//  WebViewSample
+//
+//  Created by Fumiya Tanaka on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
